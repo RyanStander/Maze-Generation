81cfaa5c3169741c540467c60937b91e2c6e9228
Ryan <stander.ryan@gmail.com>
2022-01-15 12:22:41 +0100

    Folder structure upload

---
+++ MazeGeneration/Assets/Scripts/MazeGenerator.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+/// <summary>
+/// This script will manage the generation of the maze. Taking in parameters and then displaying a generated maze.
+/// </summary>
+public class MazeGenerator : MonoBehaviour
+{
+}
